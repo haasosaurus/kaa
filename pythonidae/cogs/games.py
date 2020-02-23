bec321c456f4575de5b581c235d720fb7ae2da3f
Jim Haas <haasasaurus@gmail.com>
2020-02-22 23:31:43 -0800

    added some value checking

--- pythonidae/cogs/games.py
+++ pythonidae/cogs/games.py
@@ -15,11 +15,15 @@ class GamesCog(commands.Cog, name="Game Commands"):
     async def roll(self, ctx, number_of_dice: int = None, number_of_sides: int = None):
         number_of_dice = number_of_dice or 1
         number_of_sides = number_of_sides or 6
-        dice = [
-            str(random.choice(range(1, number_of_sides + 1)))
-            for _ in range(number_of_dice)
-        ]
-        await ctx.send(', '.join(dice))
+        if number_of_dice > 100 or number_of_sides > 1000:
+            response = 'number of dice must be <= 100, number of sides must be <= 1000'
+        else:
+            dice = [
+                str(random.choice(range(1, number_of_sides + 1)))
+                for _ in range(number_of_dice)
+            ]
+            response = '```' + ', '.join(dice) + '```'
+        await ctx.send(response)
 
 
 def setup(bot):
