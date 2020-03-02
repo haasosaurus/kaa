c5c800bede01a6fbe62d4899f90acce549fc99bb
Jim Haas <haasasaurus@gmail.com>
2020-03-01 19:03:18 -0800

    cleaned up

--- pythonidae/pythonidae.py
+++ pythonidae/pythonidae.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # coding=utf-8
 
 
@@ -10,17 +11,28 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 bot = commands.Bot(command_prefix='!')
-initial_extensions = ['cogs.documentation',
-                      'cogs.members',
-                      'cogs.owner',
-                      'cogs.games',]
 
 
-if __name__ == '__main__':
+@bot.event
+async def on_ready():
+    """http://discordpy.readthedocs.io/en/rewrite/api.html#discord.on_ready"""
+
+    print(f'Logged in as: {bot.user.name} - {bot.user.id}')
+    print(f'discord.py version: {discord.__version__}')
+    print('Servers:')
+    for guild in bot.guilds:
+        print(f'    Name: {guild.name}, Id: {guild.id}')
+
+
+def load_extensions(bot):
+    initial_extensions = [
+        'cogs.documentation',
+        'cogs.members',
+        'cogs.owner',
+        'cogs.games',
+        'cogs.resources',
+    ]
     for extension in initial_extensions:
         try:
             bot.load_extension(extension)
@@ -29,12 +41,12 @@ if __name__ == '__main__':
             traceback.print_exc()
 
 
-@bot.event
-async def on_ready():
-    """http://discordpy.readthedocs.io/en/rewrite/api.html#discord.on_ready"""
+def main(bot):
+    load_extensions(bot)
+    load_dotenv()
+    token = os.getenv('DISCORD_TOKEN')
+    bot.run(token, bot=True, reconnect=True)
 
-    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
-    print(f'Successfully logged in and booted...!')
 
-
-bot.run(TOKEN, bot=True, reconnect=True)
+if __name__ == '__main__':
+    main(bot)
