8cc6108b8c92ed1a17ca01bda75698c96c304705
Jim Haas <haasasaurus@gmail.com>
2020-03-09 17:50:04 -0700

    implemented method docs

--- pythonidae/cogs/documentation.py
+++ pythonidae/cogs/documentation.py
@@ -16,7 +16,7 @@ class DocumentationCog(commands.Cog, name="Documentation Commands"):
         self.bot = bot
 
     @commands.command(name='docs', aliases=['builtin'])
-    async def show_docs(self, ctx, cmd: str = None):
+    async def show_docs(self, ctx, cmd: str = None, mthd: str = None):
         """display documentation for a built in function or type"""
 
         usage = (
@@ -27,26 +27,33 @@ class DocumentationCog(commands.Cog, name="Documentation Commands"):
             response = usage
         elif cmd in vars(builtins):
             target = vars(builtins)[cmd]
-            method_list = [method for method in dir(target) if not method.startswith('__')]
-            if method_list:
-                methods = '\n\nMethods:\n    ' + '  '.join(method_list) + '\n'
-            else:
-                methods = ''
-            response = '**```'
-            try:
-                response_body = cmd + str(inspect.signature(target)) + '\n    ' + target.__doc__
-                response += response_body
-                if methods:
-                    response += methods
-            except (ValueError, TypeError):
-                response_body = inspect.getdoc(target)
-                if response_body:
+            if mthd:
+                if mthd in vars(target):
+                    target = vars(target)[mthd]
+                else:
+                    target = None
+                    response = '**`ERROR: method not found`**'
+            if target:
+                method_list = [method for method in dir(target) if not method.startswith('__')]
+                if method_list:
+                    methods = '\n\nMethods:\n    ' + '  '.join(method_list) + '\n'
+                else:
+                    methods = ''
+                response = '**```'
+                try:
+                    response_body = cmd + str(inspect.signature(target)) + '\n    ' + target.__doc__
                     response += response_body
                     if methods:
                         response += methods
-                else:
-                    response += 'docs not found, sorry...'
-            response += '```**'
+                except (ValueError, TypeError):
+                    response_body = inspect.getdoc(target)
+                    if response_body:
+                        response += response_body
+                        if methods:
+                            response += methods
+                    else:
+                        response += '**`docs not found, sorry...`**'
+                response += '```**'
         else:
             response = usage
         await ctx.send(response)
