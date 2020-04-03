5d8dfb6e08e7ed163a5853958a00ac67d0a4b8dc
Sam Ramirez <asdurftech@gmail.com>
2020-04-03 11:26:50 +0500

    Reverted pythonidae.py

--- pythonidae/pythonidae.py
+++ pythonidae/pythonidae.py
@@ -10,56 +10,51 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-import arkenv as env
-
 
 bot = commands.Bot(command_prefix='!')
 
 
 @bot.event
 async def on_ready() -> None:
-	"""http://discordpy.readthedocs.io/en/rewrite/api.html#discord.on_ready"""
+    """http://discordpy.readthedocs.io/en/rewrite/api.html#discord.on_ready"""
 
-	print(f'Logged in as: {bot.user.name} - {bot.user.id}')
-	print(f'discord.py version: {discord.__version__}')
-	print('Servers:')
-	for guild in bot.guilds:
-		print(f'	Name: {guild.name}, Id: {guild.id}')
+    print(f'Logged in as: {bot.user.name} - {bot.user.id}')
+    print(f'discord.py version: {discord.__version__}')
+    print('Servers:')
+    for guild in bot.guilds:
+        print(f'    Name: {guild.name}, Id: {guild.id}')
 
 
 def load_extensions(bot: commands.Bot) -> None:
-	initial_extensions = [
-		'cogs.documentation',
-		'cogs.members',
-		'cogs.owner',
-		'cogs.games',
-		'cogs.resources',
-		'cogs.troll',
-		'cogs.countdown',
-		'cogs.listener',
-		'cogs.memes'
-	]
-	for extension in initial_extensions:
-		try:
-			bot.load_extension(extension)
-		except (
-				commands.ExtensionNotFound,
-				commands.ExtensionAlreadyLoaded,
-				commands.NoEntryPointError,
-				commands.ExtensionFailed
-		):
-			print(f'Failed to load extension {extension}.', file=sys.stderr)
-			traceback.print_exc()
+    initial_extensions = [
+        'cogs.documentation',
+        'cogs.members',
+        'cogs.owner',
+        'cogs.games',
+        'cogs.resources',
+        'cogs.troll',
+        'cogs.countdown',
+        'cogs.listener',
+    ]
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except (
+                commands.ExtensionNotFound,
+                commands.ExtensionAlreadyLoaded,
+                commands.NoEntryPointError,
+                commands.ExtensionFailed
+        ):
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
 
 
 def main(bot: commands.Bot) -> None:
-	load_extensions(bot)
-	load_dotenv()
-	# token = os.getenv('DISCORD_TOKEN')
-	token = env.token
-	
-	bot.run(token, bot=True, reconnect=True)
+    load_extensions(bot)
+    load_dotenv()
+    token = os.getenv('DISCORD_TOKEN')
+    bot.run(token, bot=True, reconnect=True)
 
 
 if __name__ == '__main__':
-	main(bot)
+    main(bot)
