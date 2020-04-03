03e033fa046634689850327e88ebaf485b94d798
Jim Haas <haasasaurus@gmail.com>
2020-04-03 03:20:28 -0700

    refactored

--- pythonidae/cogs/memes.py
+++ pythonidae/cogs/memes.py
@@ -1,47 +1,61 @@
 # coding=utf-8
+
+
+import io
 import random
 
-# import discord
-from discord.ext import commands
 import discord
+from discord.ext import commands
+
+from memegenerator import MemeGenerator
 from utils import print_context
-import cogs.meme_supplements as meme_supplements
 
 
 class GamesCog(commands.Cog, name="Zi Memes"):
 	def __init__(self, bot: commands.Bot) -> None:
 		self.bot = bot
+		self.generator = MemeGenerator()
 
-	@commands.command(usage='!skill DATA')
-	async def skyrim(self, ctx: commands.Context, data):
-		
-		datasplit = data.split(",")
-		print(datasplit)
-		if (len(datasplit) >= 2):
-			skyrimIns = meme_supplements.SyrimStatusMeme(datasplit[0], datasplit[1])
-			skyrimIns.SaveFile("out.png")
-			await ctx.send(file=discord.File('out.png'))
-		else:
-			await ctx.send("Pass 2 comma-separated values to use this")
+	@commands.command(usage='!skill SKILL LEVEL')
+	@print_context
+	async def skyrim(
+			self,
+			ctx: commands.Context,
+			skill: str,
+			level: str
+	) -> None:
+		"""!skyrim SKILL LEVEL"""
 
-	@commands.command(usage='!simply THING')
-	async def simply(self, ctx: commands.Context, thing):
-		meme_supplements.ClassicMemes.oneDoesNotSimply(thing)
-		await ctx.send(file=discord.File('out.jpg'))
+		img_buf = self.generator.skyrim(skill, level)
+		await ctx.send(file=discord.File(fp=img_buf, filename='image.png'))
 
-	@commands.command(usage='!aliens THING')
-	async def aliens(self, ctx: commands.Context, thing):
-		meme_supplements.ClassicMemes.historyAliensGuy(thing)
-		await ctx.send(file=discord.File('out.jpg'))
+	@commands.command(usage='!simply *WORDS')
+	@print_context
+	async def simply(self, ctx: commands.Context, *words: str) -> None:
+		"""!simply *WORDS"""
 
-	@commands.command(usage='!toystory THING')
-	async def toystory(self, ctx: commands.Context, thing):
-		meme_supplements.ClassicMemes.toyStoryMeme(thing)
-		await ctx.send(file=discord.File('out.jpg'))
+		text = ' '.join(words)
+		img_buf = self.generator.oneDoesNotSimply(text)
+		await ctx.send(file=discord.File(fp=img_buf, filename='image.png'))
 
-	
-		
+	@commands.command(usage='!aliens *WORDS')
+	@print_context
+	async def aliens(self, ctx: commands.Context, *words: str) -> None:
+		"""!aliens *WORDS"""
+
+		text = ' '.join(words)
+		img_buf = self.generator.historyAliensGuy(text)
+		await ctx.send(file=discord.File(fp=img_buf, filename='image.png'))
+
+	@commands.command(usage='!toystory *WORDS')
+	@print_context
+	async def toystory(self, ctx: commands.Context, *words: str) -> None:
+		"""!toystory *WORDS"""
+
+		text = ' '.join(words)
+		img_buf = self.generator.toyStoryMeme(text)
+		await ctx.send(file=discord.File(fp=img_buf, filename='image.png'))
+
+
 def setup(bot: commands.Bot) -> None:
 	bot.add_cog(GamesCog(bot))
-
-
