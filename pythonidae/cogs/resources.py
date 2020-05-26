6d1145e639b174a95ccceb16d2a5fa0b6dc811d0
Jim Haas <haasasaurus@gmail.com>
2020-05-26 10:36:06 -0700

    improved discord resources

--- pythonidae/cogs/resources.py
+++ pythonidae/cogs/resources.py
@@ -222,7 +222,8 @@ __**Well liked IDEs for coding in Python**__
 
         msg = '''\
 __**discord.py resources**__
-:link: discord.py official docs <https://discordpy.readthedocs.io/en/latest/>
+:link: discord.py documentation <https://discordpy.readthedocs.io/en/latest/>
+:link: discord.ext.commands documentation <https://discordpy.readthedocs.io/en/latest/ext/commands/index.html>
 :link: realpython discord bot tutorial <https://realpython.com/how-to-make-a-discord-bot-python/>
 :link: discord.py cogs example <https://gist.github.com/OneEyedKnight/f0411f9a5e9dea23b96be0bf6dd86d2d>
 **assorted other discord.py stuff**
