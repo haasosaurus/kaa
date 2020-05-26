b709c58a03333fd1b047b96ffe66dc29fb810f40
Jim Haas <haasasaurus@gmail.com>
2020-05-26 09:48:04 -0700

    added discord.py resources

--- pythonidae/cogs/resources.py
+++ pythonidae/cogs/resources.py
@@ -215,6 +215,25 @@ __**Well liked IDEs for coding in Python**__
 '''
         await ctx.send(msg)
 
+    @commands.command(aliases=['discord.py'])
+    @print_context
+    async def discord(self, ctx: commands.Context) -> None:
+        """display discord.py learning resources"""
+
+        msg = '''\
+__**discord.py resources**__
+:link: discord.py official docs <https://discordpy.readthedocs.io/en/latest/>
+:link: realpython discord bot tutorial <https://realpython.com/how-to-make-a-discord-bot-python/>
+:link: discord.py cogs example <https://gist.github.com/OneEyedKnight/f0411f9a5e9dea23b96be0bf6dd86d2d>
+**assorted other discord.py stuff**
+:link: my discod python bot repo <https://github.com/haasosaurus/pythonidae/>
+**async stuff (you need to know a little about async for making a discord bot)**
+:link: python asyncio module docs <https://docs.python.org/3/library/asyncio.html>
+:link: python & async simplified <https://www.aeracode.org/2018/02/19/python-async-simplified/>
+:link: realpython - Async IO in Python: A Complete Walkthrough <https://realpython.com/async-io-python/>\
+'''
+        await ctx.send(msg)
+
 
 def setup(bot: commands.Bot) -> None:
     bot.add_cog(ResourceCog(bot))
