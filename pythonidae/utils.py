91be0255c4ca5bfafda94ee7bebb486289a7da85
Jim Haas <haasasaurus@gmail.com>
2020-06-04 00:19:27 -0700

    print more context info, removed unneeded stuff

--- pythonidae/utils.py
+++ pythonidae/utils.py
@@ -1,11 +1,7 @@
 # coding=utf-8
 
 
-import asyncio
 import functools
-import json
-import os
-import pathlib
 from typing import Callable
 
 from discord.ext import commands
@@ -17,16 +13,12 @@ def print_context(func: Callable) -> Callable:
         for arg in args[1:]:
             if isinstance(arg, commands.context.Context):
                 print(
-                    str(arg.author) + ': ' + str(arg.message.clean_content),
+                    f"{arg.author}: '{arg.message.clean_content}' - "
+                    f'author id: {arg.author.id}, '
+                    f"guild: '{arg.guild.name if arg.guild else 'None'}', "
+                    f"channel: '{arg.channel.name if arg.guild and arg.channel else 'None'}'",
                     flush=True
                 )
                 break
         return await func(*args, **kwargs)
     return inner
-
-
-async def load_server_dict() -> dict:
-    servers_path = pathlib.Path(os.getenv('DISCORD_SERVERS_JSON')).resolve()
-    with servers_path.open('r') as servers_file:
-        servers = json.load(servers_file)
-        return servers
