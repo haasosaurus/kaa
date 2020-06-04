8eadec1a4e8ec60054c0c2cae57b3e7eb8441837
Jim Haas <haasasaurus@gmail.com>
2020-06-04 00:14:58 -0700

    rewrote to use new settings format

--- pythonidae/cogs/listener.py
+++ pythonidae/cogs/listener.py
@@ -1,10 +1,14 @@
 # coding=utf-8
 
 
+#---------------------------- ideas ----------------------------#
+# add listener for kick
+# add listener for ban
+#---------------------------------------------------------------#
+
+
 import discord
 from discord.ext import commands
-
-from utils import load_server_dict
 
 
 class ListenerCog(commands.Cog, name='Listener'):
@@ -14,34 +18,23 @@ class ListenerCog(commands.Cog, name='Listener'):
         """initializer"""
 
         self.bot = bot
-        self.servers = None
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member) -> None:
         """welcomes new members"""
 
-        if not self.servers:
-            self.servers = await load_server_dict()
-        if not self.servers:
-            print('ERROR: server dict is empty after loading', flush=True)
-            return
-        print('SUCCESS: loaded server dict', flush=True)
+        welcome = self.bot.settings['send_welcome_default']
+        default_role = None
+        guild_settings = await self.bot.get_guild_settings(member)
+        if guild_settings:
+            welcome = guild_settings.get('send_welcome', welcome)
+            default_role = guild_settings.get('default_role', default_role)
 
-        #--- this is probably not needed ---#
-        # if not server:
-        #     print('ERROR: server not found for member', flush=True)
-        #     return
+        if welcome:
+            await member.guild.system_channel.send(f'welcome **{member.name}**')
 
-        server = member.guild
-        enabled = ['test', 'main', 'rust']
-        servers = {self.servers[x] for x in enabled if x in self.servers}
-        if server.id in servers:
-            await server.system_channel.send(f'welcome **{member.name}**')
-
-        # set default role for new members
-        default_role = 'RegularElites'  # add this to the settings file, option for each server
-        if server.id == self.servers['main']:
-            role = discord.utils.get(server.roles, name=default_role)
+        if default_role:
+            role = discord.utils.get(member.guild.roles, name=default_role)
             if role:
                 await member.add_roles(role)
 
@@ -49,23 +42,13 @@ class ListenerCog(commands.Cog, name='Listener'):
     async def on_member_remove(self, member: discord.Member) -> None:
         """says goodbye to members after they've left, pointlessly"""
 
-        if not self.servers:
-            self.servers = await load_server_dict()
-        if not self.servers:
-            print('ERROR: server dict is empty after loading', flush=True)
-            return
-        print('SUCCESS: loaded server dict', flush=True)
+        goodbye = self.bot.settings['send_goodbye_default']
+        guild_settings = await self.bot.get_guild_settings(member)
+        if guild_settings:
+            goodbye = guild_settings.get('send_goodbye', goodbye)
 
-        server = member.guild
-        if not server:
-            print('ERROR: server not found for member', flush=True)
-            return
-
-        enabled = ['test', 'main', 'rust']
-        servers = {self.servers[x] for x in enabled if x in self.servers}
-        if server.id in servers:
-            # await server.system_channel.send(f'goodbye forever {member.name}')
-            await server.system_channel.send(f'goodbye forever **{str(member)}**')
+        if goodbye:
+            await member.guild.system_channel.send(f'goodbye forever **{str(member)}**')
 
 
 def setup(bot: commands.Bot) -> None:
