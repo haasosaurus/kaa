885a6237319603e42578671bada1040a0596fbef
Jim Haas <haasasaurus@gmail.com>
2020-06-08 08:51:00 -0700

    fixed typo

--- pythonidae/cogs/resources.py
+++ pythonidae/cogs/resources.py
@@ -220,7 +220,7 @@ __**discord.py resources**__
 :link: discord.py cogs example <https://gist.github.com/OneEyedKnight/f0411f9a5e9dea23b96be0bf6dd86d2d>
 :link: error handling cog example <https://gist.github.com/pauloLuz2002/15fa6e8807245f2f41644a767188bd0a>
 **assorted other discord.py stuff**
-:link: my discod python bot repo <https://github.com/haasosaurus/pythonidae/>
+:link: my discord python bot repo <https://github.com/haasosaurus/pythonidae/>
 **async stuff (you need to know a little about async for making a discord bot)**
 :link: python asyncio module docs <https://docs.python.org/3/library/asyncio.html>
 :link: python & async simplified <https://www.aeracode.org/2018/02/19/python-async-simplified/>
