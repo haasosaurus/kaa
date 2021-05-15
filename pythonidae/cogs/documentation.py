d3b272df502a0ef2a4d4a86116ab0774b5fbed51
Jim Haas <haasasaurus@gmail.com>
2021-05-14 23:17:55 -0700

    rewrite of member analysis and pagination

--- pythonidae/cogs/documentation.py
+++ pythonidae/cogs/documentation.py
@@ -26,6 +26,7 @@ import weakref
 from typing import List, Type
 
 # third-party modules
+import aioitertools, asyncstdlib
 import matplotlib, matplotlib.pyplot, more_itertools
 import netifaces, numpy
 import pandas, PIL, PIL.ImageDraw, pretty_help, pyautogui
@@ -51,6 +52,58 @@ from utils import print_context
 
 class DocumentationCog(commands.Cog, name='documentation'):
     """python documentation commands"""
+
+    member_kind_titles_sort_order = {
+        # class specific titles and sort order
+        'methods': ('Methods', 6),
+        'methods_private': ('Methods (private)', 7),
+        'methods_class': ('Methods (class)', 8),
+        'methods_class_private': ('Methods (class) (private)', 9),
+        'methods_static': ('Methods (static)', 10),
+        'methods_static_private': ('Methods (static) (private)', 11),
+        'properties': ('Properties', 12),
+        'properties_private': ('Properties (private)', 13),
+
+        # pygame titles and sort order
+        'constants': ('Constants', 17),
+
+        # all other titles and sort order
+        'modules': ('Modules', 0),
+        'modules_private': ('Modules (private)', 1),
+        'classes': ('Classes', 2),
+        'classes_private': ('Classes (private)', 3),
+        'functions': ('Functions', 4),
+        'functions_private': ('Functions (private)', 5),
+        'exceptions': ('Exceptions', 14),
+        'data': ('Data', 15),
+        'data_private': ('Data (private)', 16),
+        'dunders': ('Dunders', 18),
+    }
+
+    @classmethod
+    async def member_kind_title(cls, kind: str) -> str:
+        """return member kind's title for output formatting"""
+
+        title_order = cls.member_kind_titles_sort_order.get(kind)
+        if title_order is None:
+            title = f"Error: member kind '{kind}' missing title and order"
+            print(title)
+        else:
+            title = title_order[0]
+        return title
+
+    @classmethod
+    async def member_kind_sort_order(cls, kind: str) -> int:
+        """return member kind's sort order for output formatting"""
+
+        title_order = cls.member_kind_titles_sort_order.get(kind)
+        if title_order is None:
+            order = 0
+            error_msg = f"Error: member kind '{kind}' missing title and order"
+            print(error_msg)
+        else:
+            order = title_order[1]
+        return order
 
     def __init__(self, bot: PythonBot) -> None:
         """initializer"""
@@ -82,6 +135,7 @@ class DocumentationCog(commands.Cog, name='documentation'):
             'weakref': weakref,
 
             # third-party modules
+            'aioitertools': aioitertools, 'asyncstdlib': asyncstdlib,
             'matplotlib': matplotlib, 'more_itertools': more_itertools,
             'netifaces': netifaces, 'numpy': numpy,
             'pandas': pandas, 'PIL': PIL, 'pretty_help': pretty_help,
@@ -109,15 +163,16 @@ class DocumentationCog(commands.Cog, name='documentation'):
             'scapy': 'scapy.all',
         }
         self.hidden_modules = {
-            'pygame_sdl2'
+            'pygame_sdl2',
         }
-        self.module_names = sorted(x for x in self.modules if x not in self.hidden_modules)
+        self.supported_modules = {
+            'title': 'Supported Modules',
+            'members': sorted(x for x in self.modules if x not in self.hidden_modules),
+        }
 
-    @commands.command(
-        aliases=['documentation', 'doc', 'pydoc', 'pydocs']
-    )
+    @commands.command(aliases=['documentation', 'doc', 'pydoc', 'docs'])
     @print_context
-    async def docs(
+    async def pydocs(
             self,
             ctx: commands.Context,
             *members: str,
@@ -151,9 +206,8 @@ class DocumentationCog(commands.Cog, name='documentation'):
                 error_msg = f"'{target_name}' has no member '{member_name}'"
                 self.bot.send_error_msg(ctx, error_msg)
                 member_list_header = f"**`{target_name}'s members:`**"
-                member_list = await self.make_members_list(target, target_name)
                 await ctx.send(member_list_header)
-                await self.send_members(ctx, member_list)
+                await self.send_members(ctx, target)
                 await self.send_usage(ctx)
                 return
             member = getattr(target, member_name)
@@ -165,16 +219,7 @@ class DocumentationCog(commands.Cog, name='documentation'):
             await self.bot.send_error_msg(error_msg)
             return
 
-        await self.send_docs_and_methods(ctx, target, target_name, '', '')
-
-    async def send_supported_modules(self, ctx: commands.Context) -> None:
-        """sends supported modules list to ctx"""
-
-        await self.send_members(
-            ctx,
-            self.module_names,
-            title='Currently available modules:'
-        )
+        await self.send_docs_and_members(ctx, target, target_name, '', '')
 
     async def send_usage(self, ctx: commands.Context) -> None:
         """send usage information"""
@@ -182,37 +227,13 @@ class DocumentationCog(commands.Cog, name='documentation'):
         usage = '**`Usage: !docs module[.| ]members...`**'
         await self.bot.send_info_msg(ctx, usage)
 
-
-
-
-
-    async def send_docs_and_methods(
-            self,
-            ctx: commands.Context,
-            target,
-            target_name: str,
-            obj=None,  # str
-            method=None  # str
-    ) -> None:
-        """final method to send everything"""
-
-        docs = await self.make_docs(target, target_name, obj, method)
-        if not docs:
-            error_msg = f'**`ERROR: docs for {target_name} not found, sorry`**'
-            await ctx.send(error_msg)
-            return
-        method_list = await self.make_members_list(target, target_name)
-        await self.send_docs(ctx, docs)
-        if method_list:
-            await self.send_members(ctx, method_list, title='Methods:')
-
     async def make_docs(
             self,
             target,
             target_name: str,
             obj=None,
             method=None
-    ) -> str:
+    ) -> List[str]:
         """make the docs and return it as a string"""
 
         if obj:
@@ -235,76 +256,246 @@ class DocumentationCog(commands.Cog, name='documentation'):
                     docs = docs_sig + '\n\n'
                 docs = docs_body
             else:
-                return ''
-        return docs
-
-    async def make_members_list(self, obj, obj_name: str) -> list:
-        """return a list of all the public members of an objest"""
-
-        if hasattr(obj, '__dict__'):
-            members = vars(obj)
+                docs = ''
+        docs = docs.strip()
+        if docs:
+            lines = docs.split('\n')
         else:
-            members = dir(obj)
-        private_or_upper = lambda x: any((x.startswith('__'), x.isupper()))
-        if obj == builtins:
-            exp = lambda x: type(vars(builtins)[x]) == type and issubclass(vars(builtins)[x], BaseException)
-            member_list = [x for x in members if not private_or_upper(x) and not exp(x)]
-        else:
-            member_list = [x for x in members if not private_or_upper(x)]
-        return tuple(member_list)
+            lines = []
+        return lines
 
-    async def send_docs(self, ctx, docs: str) -> None:
-        """send documenation string, slicing it if needed"""
-
-        if len(docs) < 1900:
-            await ctx.send('```\n' + docs + '```')
-        else:
-            docs_list = docs.strip().split('\n')
-            slices = 2
-            docs_slices = None
-            while True:
-                docs_list_slices = more_itertools.sliced(
-                    docs_list,
-                    len(docs_list) // slices
-                )
-                docs_slices = ['\n'.join(x) for x in docs_list_slices]
-                if all(len(x) < 1990 for x in docs_slices):
-                    break
-                slices += 1
-            for docs in docs_slices:
-                if not docs.isspace():
-                    msg = '```\n' + docs + '```'
-                    await ctx.send(msg)
-
-    async def send_members(
+    async def send_docs_and_members(
             self,
             ctx: commands.Context,
-            msg_list: list,
-            title: str = None
+            target,
+            target_name: str,
+            obj=None,  # str
+            method=None  # str
     ) -> None:
-        """send list of an object's members, slicing it if needed"""
+        """final method to send everything"""
 
-        msg_list = sorted(msg_list, key=lambda x: x.lower())
-        if title:
-            msg_list.insert(0, title + '\n\n')
-        msg = '```\n' + '  '.join(msg_list) + '```'
-        if len(msg) < 1990:
-            await ctx.send(msg)
+        # docs
+        lines = await self.make_docs(target, target_name, obj, method)
+        if not lines:
+            error_msg = f'**`ERROR: docs for {target_name} not found, sorry`**'
+            await ctx.send(error_msg)
+        await self.send_docs(ctx, lines)
+
+        # members
+        await self.send_members(ctx, target)
+
+    async def send_docs(self, ctx: commands.Context, lines: List[str]) -> None:
+        """paginate lines of docs and send the pages"""
+
+        pages = await self.paginate_lines(lines)
+        await self.send_pages(ctx, pages)
+
+    async def send_members(self, ctx: commands.Context, target) -> None:
+        """analyze members, paginate, and send pages"""
+
+        member_kinds = await self.analyze_members(target)
+        lines = await self.format_member_kinds(member_kinds)
+        pages = await self.paginate_lines(lines)
+        await self.send_pages(ctx, pages)
+
+    async def analyze_members(self, obj: object) -> dict:
+        """choose method to analyze and object's members with"""
+
+        if inspect.isclass(obj):
+            return await self.analyze_class_members(obj)
         else:
-            slices = 2
-            msg_slices = None
-            while True:
-                msg_list_slices = more_itertools.sliced(
-                    msg_list,
-                    len(msg_list) // slices
-                )
-                msg_slices = ['  '.join(x) for x in msg_list_slices]
-                if all(len(x) < 1970 for x in msg_slices):
-                    break
-                slices += 1
-            for msg in msg_slices:
-                msg = '```\n' + msg + '```'
-                await ctx.send(msg)
+            return await self.analyze_object_members(obj)
+
+    async def analyze_class_members(self, class_object: Type) -> dict:
+        """analyze a class's members"""
+
+        kinds = {}
+
+        # determine kind
+        attrs = sorted(inspect.classify_class_attrs(class_object), key=lambda x: x.name.lower())
+        for attr in attrs:
+            if attr.name.startswith('__'):
+                kind = 'dunders'
+            elif attr.kind == 'class method':
+                kind = 'methods_class'
+            elif attr.kind == 'static method':
+                kind = 'methods_static'
+            else:
+                kind = attr.kind
+                if kind != 'data':
+                    if kind == 'property':
+                        kind = 'properties'
+                    else:
+                        kind = f'{kind}s'
+            if kind != 'dunders' and attr.name.startswith('_'):
+                kind += '_private'
+
+            # update kinds dictionary
+            if kind in kinds:
+                kinds[kind]['members'].update({attr.name: attr.object})
+            else:
+                kinds.update({kind: {
+                    'title': await DocumentationCog.member_kind_title(kind),
+                    'members': {attr.name: attr.object}
+                }})
+
+        return kinds
+
+    async def analyze_object_members(self, obj: object) -> dict:
+        """analyze a non-class object's members"""
+
+        kinds = {}
+        members = {k: v for k, v in sorted(inspect.getmembers(obj), key=lambda x: x[0].lower())}
+
+        # separate pygame constants
+        if obj in [pygame, pygame_sdl2]:
+            kind = 'constants'
+            kinds.update({kind: {
+                'title': await DocumentationCog.member_kind_title(kind),
+                'members': {},
+            }})
+            for k, v in inspect.getmembers(pygame.constants):
+                if not k.startswith('__'):
+                    kinds[kind]['members'].update({k: v})
+                    members.pop(k, None)
+
+        # all other stuff
+        for k, v in members.items():
+            if k.startswith('__'):
+                kind = 'dunders'
+            else:
+                private = k.startswith('_')
+                if inspect.ismodule(v):
+                    if private:
+                        kind = 'modules_private'
+                    else:
+                        kind = 'modules'
+                elif inspect.isclass(v):
+                    if issubclass(v, BaseException):
+                        kind = 'exceptions'
+                    elif private:
+                        kind = 'classes_private'
+                    else:
+                        kind = 'classes'
+                elif inspect.isroutine(v):
+                    if private:
+                        kind = 'functions_private'
+                    else:
+                        kind = 'functions'
+                else:
+                    if private:
+                        kind = 'data_private'
+                    else:
+                        kind = 'data'
+            if kind in kinds:
+                kinds[kind]['members'].update({k: v})
+            else:
+                kinds.update({kind: {
+                    'title': await DocumentationCog.member_kind_title(kind),
+                    'members': {k: v},
+                }})
+
+        return kinds
+
+    async def send_supported_modules(self, ctx: commands.Context) -> None:
+        """sends supported modules list to ctx"""
+
+        lines = await self.format_member_kind(self.supported_modules, indent=4)
+        pages = await self.paginate_lines(lines)
+        await self.send_pages(ctx, pages)
+
+    async def format_member_kinds(
+            self,
+            members: dict,
+            indent: int = 4,
+            sep: str = '  ',
+            dunders: bool = False,
+            data: bool = True,
+            private: bool = False,
+            constants: bool = False,
+    ) -> List[str]:
+        """
+        input: dict of object's members organized by kind
+        output: list of formatted lines
+
+        input dict: {'kind': {'title': 'title string', 'members': []}}
+        """
+
+        lines = []
+
+        for kind in await asyncstdlib.sorted(members, key=DocumentationCog.member_kind_sort_order):
+            if kind.endswith('_private') and not private:
+                continue
+            if kind == 'dunders' and not dunders:
+                continue
+            if kind == 'constants' and not constants:
+                continue
+
+            member_kind = await self.format_member_kind(members[kind], indent, sep)
+            if lines:
+                lines.append('')
+            lines.extend(member_kind)
+
+        return lines
+
+    async def format_member_kind(
+            self,
+            member_kind: dict,
+            indent: int,
+            sep: str = '  ',
+            value: bool = False,
+    ) -> List[str]:
+        """
+        input: dict with object's specific kind of member's title and list of members
+        output: list of formatted lines
+
+        input dict: {'title': 'title string', 'members': []}
+        """
+
+        # add title and underline
+        lines = []
+        lines.append(member_kind['title'])
+        lines.append('-' * len(member_kind['title']))
+
+        # format member list
+        if '\n' in sep:
+            for k, v in member_kind['members'].items():
+                line = f"{' ' * indent}{k}"
+                if value:
+                    line += f' = {v}'
+                lines.append(line)
+        else:
+            line = ' ' * indent
+            line += sep.join(member_kind['members'])
+            lines.append(line)
+
+        return lines
+
+    async def paginate_lines(self, lines: List[str]) -> list:
+        """
+        paginate lines since discord doesn't let you send
+        messages with more than 2000 characters
+        """
+
+        if not lines:
+            return []
+        pages = []
+        page = '```'
+        while lines:
+            line = lines.pop(0)
+            if len(page) + len(line) + 1 > 1990:
+                pages.append(f'{page}```')
+                page = f'```\n{line}'
+            else:
+                page += f'\n{line}'
+        if page:
+            pages.append(f'{page}```')
+
+        return pages
+
+    async def send_pages(self, ctx: commands.Context, pages: List[str]) -> None:
+        for page in pages:
+            await ctx.send(page)
 
     @commands.command(hidden=True)
     @print_context
