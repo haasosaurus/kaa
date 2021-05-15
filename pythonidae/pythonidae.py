5c37d1233d603e7cef5aaa562e9c39367ce29c8f
Jim Haas <haasasaurus@gmail.com>
2021-05-14 23:30:16 -0700

    added some signal handling

--- pythonidae/pythonidae.py
+++ pythonidae/pythonidae.py
@@ -2,32 +2,45 @@
 # coding=utf-8
 
 
-#---------------------------- ideas ----------------------------#
-# implement catching Ctrl+c so that the bot can be shutdown
-# well and save the settings after conversion to use json
-#
-# switch print_context to use logging module
-#
-# fix packaging
-#
-# make method to call routines from coroutines
-#---------------------------------------------------------------#
-
-
+import asyncio
+from asyncio.events import AbstractEventLoop
 import os
+import signal
 
-import discord
-from discord.ext import commands
 from dotenv import load_dotenv
 
 from pythonbot import PythonBot
 
 
+async def handle_sigint(
+        signal: signal.Signals,
+        loop: AbstractEventLoop,
+        bot: PythonBot
+    ) -> None:
+    print('\n' + '-' * 30)
+    inp = input('Shutdown? [N/y] ').lower()
+    if inp.lower() in ('y', 'yes'):
+        msg = 'Shutting down, goodbye...'
+        print(msg)
+        # owner = await bot.get_owner()
+        # await bot.send_info_msg(owner, msg)
+        await bot.close()
+
 def main() -> None:
     load_dotenv()
     token = os.getenv('DISCORD_TOKEN')
     bot = PythonBot()
-    bot.run(token, bot=True, reconnect=True)
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(
+        signal.SIGINT,
+        lambda s=signal.SIGINT: asyncio.create_task(handle_sigint(s, loop, bot))
+    )
+    try:
+        loop.run_until_complete(bot.start(token, bot=True, reconnect=True))
+    except:
+        loop.run_until_complete(bot.close())
+    finally:
+        loop.close()
 
 
 if __name__ == '__main__':
