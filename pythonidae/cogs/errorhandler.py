5eb01121cd57f1a28ce32ebb79dd113783fd08da
Jim Haas <haasasaurus@gmail.com>
2021-05-15 22:44:34 -0700

    cleaned up, added some more error handling

--- pythonidae/cogs/errorhandler.py
+++ pythonidae/cogs/errorhandler.py
@@ -6,41 +6,22 @@ import sys
 import traceback
 
 # third-party modules - discord and related
-#import discord
+import discord
 from discord.ext import commands
 
 # local modules
 from pythonbot import PythonBot
 
 
-"""
-If you are not using this inside a cog, add the event decorator e.g:
-@bot.event
-async def on_command_error(ctx, error)
-
-For examples of cogs see:
-Rewrite:
-https://gist.github.com/EvieePy/d78c061a4798ae81be9825468fe146be
-Async:
-https://gist.github.com/leovoel/46cd89ed6a8f41fd09c5
-
-This example uses @rewrite version of the lib. For the async version of the lib, simply swap the places of ctx, and error.
-e.g: on_command_error(self, error, ctx)
-
-For a list of exceptions:
-http://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html#errors
-"""
-
-
 class CommandErrorHandler(commands.Cog):
+    """cog for handling errors that aren't caught by command specific error handlers"""
+
     def __init__(self, bot: PythonBot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        """The event triggered when an error is raised while invoking a command.
-        ctx   : Context
-        error : Exception"""
+    async def on_command_error(self, ctx: commands.Context, error: BaseException):
+        """triggered when an error is raised while invoking a command"""
 
         # This prevents any commands with local handlers being handled here in on_command_error.
         if hasattr(ctx.command, 'on_error'):
@@ -52,7 +33,6 @@ class CommandErrorHandler(commands.Cog):
         # If nothing is found. We keep the exception passed to on_command_error.
         error = getattr(error, 'original', error)
 
-        # Anything in ignored will return and prevent anything happening.
         if isinstance(error, ignored):
             return
 
@@ -62,48 +42,33 @@ class CommandErrorHandler(commands.Cog):
 
         elif isinstance(error, commands.NoPrivateMessage):
             try:
-                msg = f"'{ctx.command}' can not be used in Private Messages."
+                msg = f"'{ctx.command}' can't be used in private messages."
+                return await self.bot.send_error_msg(ctx, msg)
+            except:
+                pass
+
+        elif isinstance(error, commands.PrivateMessageOnly):
+            try:
+                msg = f"'{ctx.command}' can only be used in private messages."
                 return await self.bot.send_error_msg(ctx, msg)
             except:
                 pass
 
         elif isinstance(error, commands.NotOwner):
-            msg = f"'{ctx.command}' can only be used by the bot's owner."
+            msg = f"You must be the bot's owner to run '{ctx.command}'."
             return await self.bot.send_error_msg(ctx, msg)
 
         elif isinstance(error, commands.MissingRole):
             msg = f"You're missing a role required to run '{ctx.command}'."
             return await self.bot.send_error_msg(ctx, msg)
 
-        # For this error example we check to see where it came from...
-        elif isinstance(error, commands.BadArgument):
-            if ctx.command.qualified_name == 'tag list':  # Check if the command being invoked is 'tag list'
-                return await ctx.send('**`I could not find that member. Please try again.`**')
+        elif isinstance(error, commands.MissingPermissions):
+            msg = f"You don't have permission to run '{ctx.command}'."
+            return await self.bot.send_error_msg(ctx, msg)
 
         # All other Errors not returned come here... And we can just print the default TraceBack.
         print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
         traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-
-    """Below is an example of a Local Error Handler for our command do_repeat"""
-    @commands.command(name='repeat', aliases=['mimic', 'copy'], hidden=True)
-    @commands.is_owner()
-    async def do_repeat(self, ctx, *, inp: str):
-        """A simple command which repeats your input!
-        inp  : The input to be repeated"""
-
-        await ctx.send(inp)
-
-    @do_repeat.error
-    async def do_repeat_handler(self, ctx, error):
-        """A local Error Handler for our command do_repeat.
-        This will only listen for errors in do_repeat.
-
-        The global on_command_error will still be invoked after."""
-
-        # Check if our required argument inp is missing.
-        if isinstance(error, commands.MissingRequiredArgument):
-            if error.param.name == 'inp':
-                await ctx.send("You forgot to give me input to repeat!")
 
 
 def setup(bot: PythonBot) -> None:
