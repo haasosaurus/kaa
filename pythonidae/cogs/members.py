ad23bc6bf0147017265a930fe9fff02f4f9c487f
Jim Haas <haasasaurus@gmail.com>
2021-05-16 20:20:47 -0700

    aesthetic improvements

--- pythonidae/cogs/members.py
+++ pythonidae/cogs/members.py
@@ -1,10 +1,6 @@
 # coding=utf-8
 
 
-# standard library modules
-#import itertools
-#import traceback
-
 # third-party modules - discord and related
 import discord
 from discord.ext import commands
@@ -29,7 +25,6 @@ class MembersCog(commands.Cog, name='members'):
             self,
             ctx: commands.Context,
             member: discord.Member = None,
-            # *,
     ) -> None:
         """Says when a member joined."""
 
@@ -37,24 +32,47 @@ class MembersCog(commands.Cog, name='members'):
             member = ctx.message.author
 
         if member == ctx.message.author:
-            header = 'You joined:'
+            title = 'You joined:'
         else:
-            header = f"{member.display_name} joined:"
+            title = f"{member.display_name} joined:"
 
         formatted_datetime = member.joined_at.strftime('%B %d %Y at %I:%M:%S %p')
-        await self.bot.send_titled_info_msg(ctx, header, formatted_datetime)
+        await self.bot.send_titled_info_msg(ctx, title, formatted_datetime)
 
     @joined.error
     async def joined_handler(
             self,
             ctx: commands.Context,
-            error: discord.DiscordException
+            error: commands.CommandError,
     ) -> None:
         """error handling for joined command"""
 
         if isinstance(error, commands.BadArgument):
             msg = 'Member not found'
             await self.bot.send_error_msg(ctx, msg)
+
+    @commands.command(
+        help='shame tony for his laziness',
+        aliases=['search', 'lmgtfy', 'lmgt',],
+    )
+    @commands.guild_only()
+    @print_context
+    async def google(
+            self,
+            ctx: commands.Context,
+            *obvious_keywords: str
+    ) -> None:
+        """
+        generate letmegooglethat links for help vampires
+        """
+
+        if not obvious_keywords:
+            msg = '!google OBVIOUS SEARCH KEYWORDS'
+            await self.bot.send_usage_msg(ctx, msg)
+        else:
+            keywords = '+'.join(obvious_keywords)
+            msg = ':link: http://letmegooglethat.com/?q=' + keywords
+            await self.bot.send_info_msg(ctx, msg)
 
     @commands.command(hidden=True)
     @commands.guild_only()
@@ -67,25 +85,6 @@ class MembersCog(commands.Cog, name='members'):
         else:
             msg = f'Hello {ctx.author.display_name}'
         await ctx.send(msg)
-
-    @commands.command(
-        help='shame tony for his laziness',
-        aliases=['search', 'lmgtfy', 'lmgt',],
-    )
-    @commands.guild_only()
-    @print_context
-    async def google(self, ctx: commands.Context, *obvious_keywords: str) -> None:
-        """
-        generate letmegooglethat links for help vampires
-        """
-
-        if not obvious_keywords:
-            msg = '!google OBVIOUS SEARCH KEYWORDS'
-            await self.bot.send_usage_msg(ctx, msg)
-        else:
-            keywords = '+'.join(obvious_keywords)
-            msg = ':link: http://letmegooglethat.com/?q=' + keywords
-            await self.bot.send_info_msg(ctx, msg)
 
 
 def setup(bot: PythonBot) -> None:
