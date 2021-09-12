4e954f54d87100def7a98bfdbdd05edf547c94bd
Jim Haas <haasasaurus@gmail.com>
2021-09-12 16:41:42 -0700

    removed duplicates, added some unicode chars

--- pythonidae/constants.py
+++ pythonidae/constants.py
@@ -1,8 +1,21 @@
 class Colors:
     kaa = 0xfed142
-    usage = kaa
-    info = kaa
-    # error = 0xaa0000
     error = 0x7c0a02
-    # success = 0x00aa00
     success = 0x4cbb17
+
+
+class Chars:
+    U2060 = '\u2060'
+    """WORD JOINER"""
+    U202F = '\u202f'
+    """NARROW NO-BREAK SPACE"""
+    U00A0 = '\u00a0'
+    """NO BREAK SPACE"""
+    U2007 = '\u2007'
+    """FIGURE SPACE"""
+    U2011 = '\u2011'
+    """NON-BREAKING HYPHEN"""
+    U200B = '\u200b'
+    """ZERO WIDTH SPACE"""
+    U3000 = '\u3000'
+    """IDEOGRAPHIC SPACE"""
