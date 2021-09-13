a79c21cb7ba6cbc2e2a1c9d0524ab6b2bd3c1a2f
Jim Haas <haasasaurus@gmail.com>
2021-09-13 00:47:33 -0700

    reload _codeformatter

--- pythonidae/exts/code/__init__.py
+++ pythonidae/exts/code/__init__.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
+# standard library modules
+import importlib
+
+# local modules
+from . import _codeformatter
 from pythonbot import PythonBot
 
 
@@ -8,4 +13,5 @@ def setup(bot: PythonBot) -> None:
     """function the bot uses to load this extension"""
 
     from ._cog import Code
+    importlib.reload(_codeformatter)
     bot.add_cog(Code(bot))
